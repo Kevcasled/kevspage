6470364e9e00d8f35ff7e2712d2ac71668bd17d4
Kevcasled <kevincastellonledezma@gmail.com>
2026-05-22 00:40:56 +0200

    feat: test pipeline

--- backend/index.php
+++ backend/index.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 
 $data = [
     'name' => 'Kevin Castellon',
-    'role' => 'Full Stack Developer',
+    'role' => 'Full Stack Developer 2026',
     'email' => 'kevincastellonledezma@gmail.com',
     'github' => 'https://github.com/Kevcasled',
     'skills' => ['PHP', 'React', 'Laravel', 'Docker', 'Jenkins']
